2181cc02d0f926d354b5eb7f41dad9048b722808
kolby4000 <118008658+kolby4000@users.noreply.github.com>
2023-06-17 09:50:57 -0400

    Update installtomcat69.sh

--- Tomcat-installation/installtomcat69.sh
+++ Tomcat-installation/installtomcat69.sh
@@ -1,4 +1,4 @@
-# Steps for Installing tomcat9 on rhel7&8
+# Steps for Installing tomcat10 on rhel7&8
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
 sudo hostnamectl set-hostname tomcat
@@ -7,16 +7,17 @@ sudo yum install git wget vim -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 # dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
-sudo tar -xvf apache-tomcat-9.0.69.tar.gz
-sudo rm apache-tomcat-9.0.69.tar.gz
-sudo mv apache-tomcat-9.0.69 tomcat9
-sudo chmod 777 -R /opt/tomcat9
-sudo sh /opt/tomcat9/bin/startup.sh
-# create a soft link to start and stop tomcat from anywhere
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.10/bin/apache-tomcat-10.1.10.tar.gz
+sudo tar -xvf apache-tomcat-10.1.10.tar.gz
+sudo rm apache-tomcat-10.1.10.tar.gz
+sudo mv apache-tomcat-10.1.10 tomcat10
+sudo chmod 777 -R /opt/tomcat10
+sudo sh /opt/tomcat10/bin/startup.sh
+# create a soft link to start and stop tomcat from anywherecurl -v localhost:8080
+
 # to manage tomcat as a service
-sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+sudo ln -s /opt/tomcat10/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat10/bin/shutdown.sh /usr/bin/stoptomcat
 sudo starttomcat
 echo "end on tomcat installation"
 sudo su - ec2-user
